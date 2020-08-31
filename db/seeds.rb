16b754f20e12ceead0c559b7465bc06155a8dce7
JimmyLongbrow <organixaustralia@live.com>
2020-08-31 14:16:24 +1000

    'fixed password digest'

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@
 print "Creating Users... "
 User.destroy_all
 
-u1 = User.create(name:"Nemo",email:"organixaustralia@live.com", password_digest:"chicken",admin:false)
-u2 = User.create(name:"Paul",email:"paul@ge.com.au", password_digest:"chicken",admin:false)
-u3 = User.create(name:"Karthik",email:"karthik@ge.com.au", password_digest:"chicken",admin:false)
-u4 = User.create(name:"Luke",email:"luke@ge.com.au", password_digest:"chicken",admin:false)
-u5 = User.create(name:"Zara",email:"zara@ge.com.au", password_digest:"chicken",admin:false)
+u1 = User.create(name:"Nemo",email:"organixaustralia@live.com", password:"chicken",admin:false)
+u2 = User.create(name:"Paul",email:"paul@ge.com.au", password:"chicken",admin:false)
+u3 = User.create(name:"Karthik",email:"karthik@ge.com.au", password:"chicken",admin:false)
+u4 = User.create(name:"Luke",email:"luke@ge.com.au", password:"chicken",admin:false)
+u5 = User.create(name:"Zara",email:"zara@ge.com.au", password:"chicken",admin:false)
 
 
 puts "created #{ User.count } Users."
