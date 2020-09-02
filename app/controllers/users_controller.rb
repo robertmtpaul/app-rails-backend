0cd52b2570e8390a08fe387a061c720700362a37
JimmyLongbrow <organixaustralia@live.com>
2020-09-02 19:49:31 +1000

    'create user'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,22 +8,32 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
-    render json:User.all
+    # render json:User.all
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
   def new
     @user = User.new
-    render json:User.new
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # GET /users/1/edit
   def edit
+    @user = User.find(params[:id])
   end
 
   # POST /users
@@ -45,6 +55,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+    @user = User.find(params[:id])
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
@@ -74,6 +85,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :password_digest, :profile_pic, :admin)
+      params.require(:user).permit(:name, :password_digest, :email, :profile_pic, :admin)
     end
 end
