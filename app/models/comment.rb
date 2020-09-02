3ec9a27df682087da75bef50977157ae9a308cf4
robertmtpaul <rob@glo.id.au>
2020-09-02 18:20:17 +1000

    Merge to master

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ApplicationRecord
-<<<<<<< HEAD
     belongs_to :post, optional:true
     belongs_to :user
     belongs_to :post_photo, optional:true
@@ -22,9 +21,4 @@ class Comment < ApplicationRecord
     def already_liked_by?(user)
       Like.where(user:user, target_id:self.id, target_type:'post').any?
     end
-=======
-    belongs_to :post, optional: true
-    belongs_to :users, optional: true
-    has_many :likes
->>>>>>> 4d63ce4c09d855a17ed1090987421f007a1ddde7
 end
