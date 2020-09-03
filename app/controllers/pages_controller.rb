8d9b5de02b7f8d6ed80f109cafcd123919513986
JimmyLongbrow <organixaustralia@live.com>
2020-09-03 17:03:55 +1000

    'admin'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
-  # TODO : re-enable check once redirecting non-logged in users... 
+  # TODO : re-enable check once redirecting non-logged in users...
   before_action :check_if_logged_in, only: [:home]
-  before_action :check_if_admin, only: [:home]
-  
+  # before_action :check_if_admin, only: [:home]
+
   def welcome
   end
-  
+
   def home
   end
 end
